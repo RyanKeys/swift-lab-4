e610c4ec2b1a53b1c419f3af73c2c4f0718c04ca
Ryan Keys <r.keys1998@gmail.com>
2020-11-09 22:23:10 -0800

    init

--- Lab4.playground/Pages/Structs - App Exercise.xcplaygroundpage/Contents.swift
+++ Lab4.playground/Pages/Structs - App Exercise.xcplaygroundpage/Contents.swift
@@ -5,30 +5,37 @@ import Foundation
 
 For most apps you'll need to have a data structure to hold information about a user. Create a `User` struct that has properties for basic information about a user. At a minimum, it should have properties to represent a user's name, age, height, weight, and activity level. You could do this by having `name` be a `String`, `age` be an `Int`, `height` and `weight` be of type `Double`, and `activityLevel` be an `Int` that will represent a scoring 1-10 of how active they are. Implement this now.
 */
-
+struct User {
+    var name:String = ""
+    var age:Int = 0
+    var height:Double = 0.00
+    var weight:Double = 0.00
+    var activityLevel:Double = 0.00
+}
 
 /*:
 Create a variable instance of `User` and call it your name. Use the memberwise initializer to pass in information about yourself. Then print out a description of your `User` instance using the instance's properties.
 */
-
-
+var Ryan = User(name: "Ryan", age: 22, height: 6.00, weight: 200.00, activityLevel: 0.00)
 /*:
 Distance can be represented using a variety of units of measurement. Create a `Distance` struct that will represent distance in various units of measurement. At a minimum, it should have a `meters` property and a `feet` property. Create a custom initializer corresponding to each property (i.e. if you only have the two properties for meters and feet you will then have two initializers) that will take in a distance in one unit of measurement and assign the correct value to both units of measurements. Hint: *1 meter = 3.28084 feet*.
 
 - Example: If you use the initializer for meters and pass in a distance of 1600, the initializer should set `meters` to 1600 and `feet` to 5249.344.
 */
-
+struct Distance {
+    var meters:Float = 0.0
+    var feet:Double = 5249.344
+}
 
 /*:
 Now create an instance of `Distance` called `mile`. Use the initializer for meters to set the distance to 1600. Print out the property for feet and verify that it is equal to 5249.344.
 */
-
-
+var mile = Distance(meters: 1600)
+print(mile.feet)
 /*:
 Now create another instance of `Distance` and give it some other distance. Ensure that both properties are set correctly.
 */
-
-
+var otherDis = Distance(meters: 0.0, feet: 0.00)
 /*:
 ## Mile Times and Congratulations
 
@@ -40,21 +47,35 @@ struct RunningWorkout {
    var distance: Double
    var time: Double
    var elevation: Double
+    mutating func averageMileTime() -> Double {
+        return distance/time
+    }
 }
+
+
+var workout = RunningWorkout(distance: 1.00, time: 60.00, elevation: 100.00)
+print(workout.averageMileTime())
 /*:
 In other app exercises, you've provided encouraging messages to the user based on how many steps they've completed. A great place to check whether or not you should display something to the user is in a property observer.
 
 In the `Steps` struct below, add a `willSet` to the `steps` property that will check if the new value is equal to `goal`, and if it is, prints a congratulatory message. Create an instance of `Steps` where `steps` is 9999 and `goal` is 10000, then call `takeStep()` and see if your message is printed to the console.
 */
 struct Steps {
-   var steps: Int
+    var steps: Int {
+        willSet {
+            print(steps)
+         if newValue == goal {
+             print("Good Job!")
+         }
+        }
+    }
    var goal: Int
-   
    mutating func takeStep() {
        steps += 1
    }
 }
-
+var step = Steps(steps: 999, goal: 1000)
+step.takeStep()
 /*:You can refer to this resource to read about Property Observers:
  https://www.hackingwithswift.com/read/0/17/properties
  */
--- Lab4.playground/Pages/Structs - Methods.xcplaygroundpage/Contents.swift
+++ Lab4.playground/Pages/Structs - Methods.xcplaygroundpage/Contents.swift
@@ -10,7 +10,12 @@ struct Book {
     var author: String
     var pages: Int
     var price: Double
+    mutating func description() -> String {
+        return title + " by " + author + "\nPages: " + String(pages) + "\nPrice: $" + String(price)
+    }
 }
+var b = Book(title: "Harry Potter", author: "Jk Rowling", pages: 999, price: 19.99)
+print(b .description())
 /*:
  A `Post` struct has been created for you below, representing a generic social media post. Add a mutating method on `Post` called `like` that will increment `likes` by one. Then create an instance of `Post` and call `like()` on it. Print out the `likes` property before and after calling the method to see whether or not the value was incremented.
  */
@@ -18,5 +23,11 @@ struct Post {
     var message: String
     var likes: Int
     var numberOfComments: Int
+    mutating func like() {
+        likes+=1
+    }
 }
+
+var p = Post(message: "Yo", likes: 0, numberOfComments: 0)
+p.like()
 //: [Next](@next)
